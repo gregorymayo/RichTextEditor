f4ffa31871bd9ec81afe54f0c66f7f7b25240ebe
gregorymayo <gregoryjuevesii23@gmail.com>
2021-12-04 01:06:30 -0800

    fix npm run build error

--- pages/App.js
+++ pages/App.js
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react'
-import { createEditor } from 'slate'
+import { createEditor, Editor, Transforms, Text } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 import DefaultElement from './textElement/DefaultElement.js'
 import CodeElement from './textElement/CodeElement.js'
@@ -7,7 +7,6 @@ import LeftAlign from './textAlign/LeftAlign.js'
 import CenterAlign from './textAlign/CenterAlign.js'
 import RightAlign from './textAlign/RightAlign.js'
 import CodeStyle from './textStyle/CodeStyle.js'
-import { CustomEditor } from './CustomEditor.js'
 import { Button } from 'antd'
 import {
     BoldOutlined,
@@ -20,6 +19,110 @@ import {
 
 } from '@ant-design/icons'
 import styles from '../styles/Home.module.css'
+
+const CustomEditor = {
+    isBoldMarkActive(editor) {
+        // A root-level Editor node that contains the entire document's content.
+        const [match] = Editor.nodes(editor, {
+            match: n => n.bold === true
+        })
+        // !! way of casting a "truthy" or "falsy"
+        return !!match
+    },
+    toggleBoldMark(editor) {
+        const isActive = CustomEditor.isBoldMarkActive(editor)
+        // Transforms are functions that let us change the editor's value.
+        Transforms.setNodes(
+            editor,
+            { bold: isActive ? null : true },
+            { match: n => Text.isText(n), split: true }
+        )
+    },
+    isItalicMarkActive(editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.italic === true
+        })
+        return !!match
+    },
+    toggleItalicMark(editor) {
+        const isActive = CustomEditor.isItalicMarkActive(editor)
+        Transforms.setNodes(
+            editor,
+            { italic: isActive ? null : true },
+            { match: n => Text.isText(n), split: true }
+        )
+    },
+    isUnderlineMarkActive(editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.underline === true
+        })
+        return !!match
+    },
+    toggleUnderlineMark(editor) {
+        const isActive = CustomEditor.isUnderlineMarkActive(editor)
+        Transforms.setNodes(
+            editor,
+            { underline: isActive ? null : true },
+            { match: n => Text.isText(n), split: true }
+        )
+    },
+    isCodeBlocMarkActive(editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.type === 'code'
+        })
+        return !!match
+    },
+    toggleCodeBlockMark(editor) {
+        const isActive = CustomEditor.isCodeBlocMarkActive(editor)
+        Transforms.setNodes(
+            editor,
+            { type: isActive ? 'paragraph' : 'code' },
+            { match: n => Editor.isBlock(editor, n) }
+        )
+    },
+    isLeftAlignTextActive(editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.type === 'left'
+        })
+        return !!match
+    },
+    toggleLeftAlignTextMark(editor) {
+        const isActive = CustomEditor.isLeftAlignTextActive(editor)
+        Transforms.setNodes(
+            editor,
+            { type: isActive ? 'paragraph' : 'left' },
+            { match: n => Editor.isBlock(editor, n) }
+        )
+    },
+    isCenterAlignTextActive(editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.type === 'center'
+        })
+        return !!match
+    },
+    toggleCenterAlignTextMark(editor) {
+        const isActive = CustomEditor.isCenterAlignTextActive(editor)
+        Transforms.setNodes(
+            editor,
+            { type: isActive ? 'paragraph' : 'center' },
+            { match: n => Editor.isBlock(editor, n) }
+        )
+    },
+    isRightAlignTextActive(editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.type === 'right'
+        })
+        return !!match
+    },
+    toggleRightAlignTextMark(editor) {
+        const isActive = CustomEditor.isRightAlignTextActive(editor)
+        Transforms.setNodes(
+            editor,
+            { type: isActive ? 'paragraph' : 'right' },
+            { match: n => Editor.isBlock(editor, n) }
+        )
+    }
+}
 
 const App = () => {
     const editor = useMemo( () => withReact(createEditor()), []) // Slate editor object
